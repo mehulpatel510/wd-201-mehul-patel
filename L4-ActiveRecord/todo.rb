15cf71d2d1e7801961d9ae778d8812c2936ec55c
mehulpatel510 <mspatel.it@spcevng.ac.in>
2021-09-12 00:05:47 +0530

    Improved Todo Management CLI app

--- L4-ActiveRecord/todo.rb
+++ L4-ActiveRecord/todo.rb
@@ -15,19 +15,31 @@ class Todo < ActiveRecord::Base
         all.map {|todo| todo.to_displayable_string }
     end
 
+    def self.overdue
+        where("due_date < ?", Date.today)
+    end
+
+    def self.due_today
+        where("due_date = ?", Date.today)
+    end
+
+    def self.due_later
+        where("due_date > ?", Date.today)
+    end
+
     def self.show_list
         puts "My Todo-list\n\n"
 
         puts "Overdue\n"
-        puts Todo.where(" due_date < ? ", Date.today ).to_displayable_list
+        puts overdue.map { |todo| todo.to_displayable_string }
         puts "\n\n"
         
         puts "Due Today\n"
-        puts Todo.where(" due_date = ? ", Date.today ).to_displayable_list
+        puts due_today.map { |todo| todo.to_displayable_string }
         puts "\n\n"
         
         puts "Due Later\n"
-        puts Todo.where(" due_date > ? ", Date.today ).to_displayable_list
+        puts due_later.map { |todo| todo.to_displayable_string }
         puts "\n\n"
     end
 
